5ae800cf9fd8a102d80d92f59ee1ebd1ae108c93
Biomutantz <104140348+Biomutantz@users.noreply.github.com>
2023-03-01 13:44:47 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,350 @@
+// code for team page access
+
+const mainPage = document.querySelector(".main__container");
+const teamPageAccess = document.querySelector(".team__page");
+const teamButton = document.querySelector(".team__button");
+const footer = document.querySelector(".footer");
+const credit = document.querySelector(".credit");
+const contact = document.querySelector(".contact");
+const modalPage = document.querySelector(".modal__page");
+// teamButton.addEventListener("click", (e)=> {
+//     e.preventDefault();
+//     mainPage.classList.add("hide");
+//     footer.classList.add("hide");
+//     contact.classList.add("hide");
+//     credit.classList.add("hide");
+//     teamPageAccess.classList.remove("hide");
+// })
+
+// tech button 1
+
+const tech1 = document.querySelector(".techd");
+const tech_page1 = document.querySelector(".tech_page1");
+const closeBtn = document.querySelector(".c");
+
+tech1.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page1.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page1.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+
+// tech button 2
+
+const tech2 = document.querySelector(".tech2");
+const tech_page2 = document.querySelector(".tech_page2");
+const closeBtn2 = document.querySelector(".c");
+
+tech2.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page2.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page2.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+
+// tech button 3
+
+const tech3 = document.querySelector(".tech3");
+const tech_page3 = document.querySelector(".tech_page3");
+
+
+tech3.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page3.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page2.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+
+// tech button 4
+
+const tech4 = document.querySelector(".tech4");
+const tech_page4 = document.querySelector(".tech_page4");
+
+
+tech4.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page4.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page4.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 5
+
+const tech5 = document.querySelector(".tech5");
+const tech_page5 = document.querySelector(".tech_page5");
+
+
+tech5.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page5.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page5.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 6
+
+const tech6 = document.querySelector(".tech6");
+const tech_page6 = document.querySelector(".tech_page6");
+
+
+tech6.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page6.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page6.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 7
+
+const tech7 = document.querySelector(".tech7");
+const tech_page7 = document.querySelector(".tech_page7");
+
+
+tech7.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page7.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page7.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 8
+
+const tech8 = document.querySelector(".tech8");
+const tech_page8 = document.querySelector(".tech_page8");
+
+
+tech8.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page8.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page8.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 9
+
+const tech9 = document.querySelector(".tech9");
+const tech_page9 = document.querySelector(".tech_page9");
+
+
+tech9.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page9.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page9.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 10
+
+const tech10 = document.querySelector(".tech10");
+const tech_page10 = document.querySelector(".tech_page10");
+
+
+tech10.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page10.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page10.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 11
+
+const tech11 = document.querySelector(".tech11");
+const tech_page11 = document.querySelector(".tech_page11");
+
+
+tech11.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page11.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page11.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 12
+
+const tech12 = document.querySelector(".tech12");
+const tech_page12 = document.querySelector(".tech_page12");
+
+
+tech12.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page12.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page12.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 13
+
+const tech13 = document.querySelector(".tech13");
+const tech_page13 = document.querySelector(".tech_page13");
+
+
+tech13.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page13.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page13.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 14
+
+const tech14 = document.querySelector(".tech14");
+const tech_page14 = document.querySelector(".tech_page14");
+
+
+tech14.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page14.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page14.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
+
+// tech button 15
+
+const tech15 = document.querySelector(".tech15");
+const tech_page15 = document.querySelector(".tech_page15");
+
+
+tech15.addEventListener("click", ()=> {
+    mainPage.classList.add("hide");
+    footer.classList.add("hide");
+    contact.classList.add("hide");
+    credit.classList.add("hide");
+    tech_page15.classList.remove("hide");
+})
+
+closeBtn.addEventListener("click", ()=> {
+    tech_page15.classList.add("hide");
+    mainPage.classList.remove("hide");
+    footer.classList.remove("hide");
+    contact.classList.remove("hide");
+    credit.classList.remove("hide");
+})
